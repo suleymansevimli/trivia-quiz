a9e9d94c548c757bd319e688bbabdb3857bb7d0d
suleyman <suleyman.sevimli1@gmail.com>
2021-04-04 22:46:44 +0300

    editor edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {ChangeEvent, useState} from 'react';
+import {GlobalStyle} from "./styles/Global.styles";
+import Login from "./components/screen/Login";
+import Quiz from "./components/screen/Quiz";
+import {Difficulty, fetchQuizQuestions} from "./helpers/Api";
+import Button from "./components/Form/Button";
+
+const TOTAL_QUESTIONS = 10;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [name, setName] = useState<string>("")
+    const [gameIsOn, setGameIsOn] = useState<boolean>(false)
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+    const [fieldsCheck, setFieldsCheck] = useState<boolean>(true)
+    const [quizData, setQuizData] = useState<[]>([])
+
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value);
+        setFieldsCheck(!((e.target.value).length > 2))
+    }
+
+    const onSubmit = (e: React.MouseEvent<HTMLButtonElement>) => setIsLoggedIn(true);
+
+    const fetchQuizData = React.useCallback(async () => {
+        return await fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY);
+    }, [])
+
+    const startQuiz = (e: React.MouseEvent<HTMLButtonElement>) => {
+        fetchQuizData()
+            .then(data => {
+                setQuizData(data)
+                setGameIsOn(true);
+            })
+            .catch(e => console.log(e))
+    }
+
+    return (
+        <div style={{flex: 1}}>
+            <GlobalStyle/>
+            {!isLoggedIn && <Login fieldsCheck={fieldsCheck} handleInput={handleInput} name={name} onSubmit={onSubmit}/>}
+            {isLoggedIn && !gameIsOn && <Button onClick={startQuiz} title={"Start"}/>}
+            {gameIsOn && <Quiz nickname={name} questions={quizData}/>}
+        </div>
+    );
 }
 
 export default App;
